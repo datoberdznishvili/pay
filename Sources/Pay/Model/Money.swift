969f9326e2fb7beca0585116f1fa749089ceff56
Giga Khizanishvili <khizanag@gmail.com>
2024-08-21 06:48:24 +0400

    Fix amount format

--- Sources/Pay/Model/Money.swift
+++ Sources/Pay/Model/Money.swift
@@ -5,6 +5,8 @@
 //  Created by Giga Khizanishvili on 02.07.24.
 //
 
+import Foundation
+
 public struct Money {
     let amount: Double
     let currency: Currency
@@ -17,11 +19,28 @@ public struct Money {
 
 extension Money {
     func formatted() -> String {
+        func formatNumber(_ number: Double) -> String {
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal
+            formatter.groupingSeparator = " "
+            formatter.decimalSeparator = "."    // Ensure a dot as the decimal separator
+            formatter.maximumFractionDigits = 2
+            formatter.minimumFractionDigits = 2
+
+            if let formattedNumber = formatter.string(from: NSNumber(value: number)) {
+                return formattedNumber
+            } else {
+                return "\(number)"
+            }
+        }
+
+        let formattedAmount = formatNumber(amount)
+
         switch currency.position {
         case .beforeAmount:
-            "\(currency.symbol)\(amount)"
+            return "\(currency.symbol)\(formattedAmount)"
         case .afterAmount:
-            "\(amount) \(currency.symbol)"
+            return "\(formattedAmount) \(currency.symbol)"
         }
     }
 }
